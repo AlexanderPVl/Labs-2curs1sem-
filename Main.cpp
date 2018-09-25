5e2310ce16bcaa12a720908755395d5aa408c29e
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-09-26 01:19:36 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main()
 {
 	Cube cube1(3, 2);
-	Vector2 vect(0, 0, 4, 3);
+	Vector2 vect(0, 0, 2, 1);
 	cout << cube1.MaxCube() << endl;
 	cout << vect.VectorLen() << endl;
+	vect.FindNormal();
+	cout << vect.GetCoordArray(0) << " " << vect.GetCoordArray(1) << " " << vect.GetCoordArray(2) << " " << vect.GetCoordArray(3) << endl;
 	return 0;
 }
