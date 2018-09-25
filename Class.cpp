dcf69b284dead8074b0235a8220458e856d446bd
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-09-26 01:20:01 +0300

    Update Class.cpp

--- Class.cpp
+++ Class.cpp
@@ -27,29 +27,73 @@ int Cube::MaxCube(){
 }
 
 //VECTOR==============================
+void Vector2::FindNormal(){
+	if (!d('x') && !d('y')){
+		Coord[0] = Coord[1] = Coord[2] = Coord[3] = 0;
+		return;
+	}
+	if (!d('x')){
+		Coord[0] = Coord[1] = Coord[3] = 0;
+		Coord[2] = 1;
+		return;
+	}
+	else if (!d('y')){
+		Coord[0] = Coord[1] = Coord[2] = 0;
+		Coord[3] = 1;
+		return;
+	}
+	else{
+		Coord[0] = Coord[1] = 0;
+		Coord[2] = 1 / d('x');
+		Coord[3] = -1 / d('y');
+		return;
+	}
+
+	Coord[0] = 1;
+	Coord[1] = 1;
+	Coord[2] = 1;
+	Coord[3] = 1;
+}
 Vector2::Vector2(double x1, double y1, double x2, double y2){
+	X1 = x1;
+	Y1 = y1;
+	X2 = x2;
+	Y2 = y2;
 	Coord = new double[4];
-	Coord[0] = x1; Coord[1] = y1;
-	Coord[2] = x2; Coord[3] = y2;
 }
 Vector2::~Vector2(){
 	delete[] Coord;
 }
 
 double& Vector2::GetCoordRef(int i){
-	return Coord[i];
+	switch (i){
+	case 0: return X1;
+	case 1: return Y1;
+	case 2: return X2;
+	case 3: return Y2;
+	default: return X1;
+	}
 }
 double Vector2::GetCoord(int i) const{
-	return Coord[i];
+	switch (i){
+	case 0: return X1;
+	case 1: return Y1;
+	case 2: return X2;
+	case 3: return Y2;
+	default: return 0;
+	}
 }
 void Vector2::SetCoord(int i, double val){
 	GetCoordRef(i) = val;
 }
 double Vector2::d(const char var) const{
-	if (var == 'x')return GetCoord(2) - GetCoord(0);
-	if (var == 'y')return GetCoord(3) - GetCoord(1);
+	if (var == 'x')return (GetCoord(2) - GetCoord(0));
+	if (var == 'y')return (GetCoord(3) - GetCoord(1));
 	else return 0;
 }
 double Vector2::VectorLen() const{
 	return sqrt(pow(d('x'), 2) + pow(d('y'), 2));
+}
+double Vector2::GetCoordArray(int i){
+	return Coord[i];
 }
