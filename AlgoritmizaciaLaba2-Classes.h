5183dbf2d212f02b64dae7b5d2deb7a6d840c9e4
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-10-02 17:30:18 +0300

    Update AlgoritmizaciaLaba2-Classes.h

--- AlgoritmizaciaLaba2-Classes.h
+++ AlgoritmizaciaLaba2-Classes.h
@@ -44,7 +44,62 @@ public:
 	int GetCount();
 	Type& ArrAccess();
 	Type& operator [] (int n);
+	ostream& Container<Type>::operator << (ostream &ost){
+		int i = 0;
+		for (; i < Count; i++)
+			cout << Array[i];
+		return ost;
+	}
 };
+
+//CONTAINER======================================
+template <class Type>
+Type& Container<Type>::operator [] (int n){
+	if (n >= Count)
+		return Array[Count];
+	return Array[n];
+}
+template <class Type>
+Container<Type>::~Container(){
+	delete[] Array;
+}
+template <class Type>
+Container<Type>::Container(Container<Type>&Cont){
+	int i;
+	Array = new Type[Cont.GetCount()];
+	for (i = 0; i < Cont.GetCount(); i++){
+		Array[i] = Cont[i];
+	}
+	this->Count = Cont.GetCount();
+}
+template <class Type>
+Container<Type>::Container(int N){
+	Array = new Type[N];
+	this->Count = N;
+}
+template <class Type>
+Container<Type>::Container(){
+	Array = new Type[1];
+	this->Count = 1;
+}
+/*template <class Type>
+void Container<Type>::Initialise(){
+Type T;
+int i = 0;
+for (; i < Count; i++)
+Array[i] = T;
+}*/
+template <class Type>
+int Container<Type>::GetCount(){
+	return Count;
+}
+template <class Type>
+Type& Container<Type>::ArrAccess(){
+	return Array;
+}
+//template <class Type>
+
+
 
 ostream &operator << (ostream &ost, Fract f);
 //template<class Type>
