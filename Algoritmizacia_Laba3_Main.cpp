44931f3f32d3ff6ee09615934e41a8f989f28eed
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-10-09 18:04:22 +0300

    Update Algoritmizacia_Laba3_Main.cpp

--- Algoritmizacia_Laba3_Main.cpp
+++ Algoritmizacia_Laba3_Main.cpp
@@ -4,6 +4,7 @@ int main(){
 	//Player pl(10);
 	Developer Dev(1, 1, "Mark", "AlienLang");
 	Dev.PrintInfo();
+	Dev.PrintSomeCode();
 	int a = 123567;
 	//cout << to_string(a).length() << endl;
 	return 0;
