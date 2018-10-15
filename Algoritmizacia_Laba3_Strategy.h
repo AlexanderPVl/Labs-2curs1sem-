7e32457ae9681d5fb03f71526804b30498661bd3
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-10-15 22:36:20 +0300

    Update Algoritmizacia_Laba3_Strategy.h

--- Algoritmizacia_Laba3_Strategy.h
+++ Algoritmizacia_Laba3_Strategy.h
@@ -1,50 +1,105 @@
-#include <iostream>
+#pragma once
+
 #include <vector>
-#include <string>
+#include <iomanip>
+#include <time.h>
 
-using namespace std;
+#include "Includes.h"
 
-typedef vector <int> IntVec;
+#define PI 3.141592653589793
+
+void ResetGlobalTime();
+
+class Vector2{
+public:
+	double x;
+	double y;
+
+	Vector2();
+	Vector2(double x_, double y_);
+	void Set(double x_ = 0, double y_ = 0);
+	double Length();
+	Vector2& Normalize();
+	Vector2& Rotate(double deg);
+
+	Vector2& operator += (const Vector2& v);
+	Vector2& operator = (const Vector2& v);
+	std::ostream& operator << (std::ostream& ost);
+};
+//VECTOR2 operators
+//==============================<
+Vector2 operator + (const Vector2& v1, const Vector2& v2);
+Vector2 operator - (const Vector2& v1, const Vector2& v2);
+Vector2 operator * (const Vector2& v1, const double k);
+std::ostream& operator << (std::ostream& ost, const Vector2& v);
+bool operator == (const Vector2& v1, const Vector2& v2);
+//==============================/>
 
 class Player{
 protected:
-	typedef struct{
-		int X;
-		int Y;
-	} Coordinates;
-	Coordinates Coord;
-	int ID;
-	int HP;
-	string Name;
+	int id;
+	std::string name;
 public:
-	Player(int id, int hp, string name){
-		Coord = { 0, 0 };
-		ID = id;
-		HP = hp;
-		Name = name;
-	}
-	void MoveNor(){ ++Coord.Y; }
-	void MoveSou(){ --Coord.Y; }
-	void MoveWes(){ --Coord.X; }
-	void MoveEas(){ ++Coord.X; }
+	Player(int id, std::string& name);
 
-	virtual void PrintInfo() = 0{
-		cout << "Player name: " << Name;
-	}
+	virtual void PrintInfo() = 0;
 };
 
-class Developer : protected Player{
+class Unit{
+public:
+	Unit(){}
+	virtual void Hit(int) {}
+	virtual void Attac(Unit&) {}
+	virtual Vector2& Move(double) = 0 {}
+	virtual Vector2& Rotate(int) = 0 {}
+	virtual Vector2& GetPosition() = 0 {}
+	virtual Vector2& GetDirection() = 0 {}
+	virtual void GoToPoint(Vector2&) = 0 {}
+
+	virtual void PrintInfo() = 0 {}
+	virtual ~Unit() {}
+};
+
+class UFC_Unit : public Unit{
 protected:
-	string Language;
+	int id;
+	int hp;
+	int attac;
+	bool alive;
+	Vector2 position;
+	Vector2 direction;
+
+	time_t attac_delay;
+	time_t last_attac_time;
+
+	double speed;
 
 public:
-	Developer(int id, int hp, string name, string lang) : Player(id, hp, name){
-		Language = lang;
-	}
-	void PrintSomeCode(){
-		cout << "int main(){" << endl << "\tHello, world!;" << endl << "\treturn 0;" << endl << "}" << endl;
-	}
-	void PrintInfo() override{
-		cout << "Developer name: " << Name << endl;
-	}
+
+	void Hit(int n) final override;
+	void Attac(Unit& u) override;
+	Vector2& Move(double) final override;
+	Vector2& Rotate(int) final override;
+	Vector2& GetPosition() final override;
+	Vector2& GetDirection() final override;
+	void GoToPoint(Vector2& point) final override;
+
+	void PrintInfo() override;
+};
+
+class Connor : public UFC_Unit{
+public:
+	Connor();
+};
+
+class Habib : public UFC_Unit{
+public:
+	Habib();
+};
+
+class Ferguson : public UFC_Unit{
+	int krit;
+public:
+	Ferguson();
+	void Attac(Unit& u) final override;
 };
