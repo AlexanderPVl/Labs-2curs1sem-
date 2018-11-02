5eb50a13df2af87a7fe5f438b8ecef51b3a8c8ae
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-11-02 07:11:33 +0300

    Update exceptions.h

--- Komp_Prac_3/exceptions.h
+++ Komp_Prac_3/exceptions.h
@@ -9,8 +9,21 @@ using namespace std;
 class except_empty_container : public exception {
 public:
 	except_empty_container() : exception("Container is empty") {}
-	except_empty_container(const char* msg) : exception(msg) {}
+	except_empty_container(const char* msg) : exception(msg) { cout << "\n" << msg << endl; }
+	except_empty_container(const char* msg, bool if_empty_, int size_) : exception(msg) {
+		cout << "\n" << msg << endl;
+		if_empty = if_empty_;
+		size = size_;
+	}
 private:
+	bool if_empty;
+	int size;
+};
+
+class except_non_compatible : public exception {
+public:
+	except_non_compatible() : exception("Types are not compatible") {}
+	except_non_compatible(const char* msg) : exception(msg) { cout << "\n" << msg << endl; }
 };
 
 #endif
