8eabee084b25232910b847331df130db72a1b845
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2019-02-26 05:24:21 +0300

    Create main(4alg).cpp

---
+++ main(4alg).cpp
@@ -0,0 +1,66 @@
+#include "bin_tree.h"
+#include <algorithm>
+
+//using namespace std;
+
+void bin_tree_fucn(){
+	node<int, char> nd1;
+	nd1.data.key = 10; nd1.data.val = 'a';
+	binary_tree<int, char> bt;
+	//bt.add(nd1);
+	bt.add(10, 'a');
+	bt.add(8, 'b');
+	bt.add(7, 'c');
+	bt.add(9, 'u');
+	bt.add(12, 'v');
+	bt.add(11, 'x');
+	bt.add(13, 'w');
+
+	//print_tree(&base(bt));
+
+	bt.range_for([](node<int, char> &nd){ print(nd); }, &base(bt), 8, 11);
+	bt.for_each([](node<int, char> &nd){ print(nd); }, &base(bt));
+
+	binary_tree<int, char>::iterator t;
+	//print(*t);
+	bt.delete_tree(&base(bt));
+}
+
+void stack_func(){
+	stack<int> st;
+	int a;
+	for (int i = 1; i <= 17; ++i){
+		st.push(i);
+	}
+
+	std::for_each(st.begin(), st.end(), [](int t){std::cout << t << " "; });
+
+	for (int i = 1; i <= 4; ++i){
+		st.pop();
+	}
+	st.delete_excess();
+	std::cout << std::endl;
+	std::for_each(st.last(), st.end(), [](int t){std::cout << t << " "; });
+
+	st.delete_all();
+}
+
+void queue_func(){
+	queue<int> q;
+
+	for (int i = 1; i <= 4; ++i){
+		q.push(i);
+	}
+	for (int i = 0; i < 4; ++i){
+		std::cout << q.pop();
+	}
+	q.print();
+}
+
+int main(){
+	//bin_tree_fucn();
+	//stack_func();
+	queue_func();
+
+	return 0;
+}
