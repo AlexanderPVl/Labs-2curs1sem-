bb54745d4ca62c357fa77189cb43180815efb352
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2019-03-04 15:33:57 +0200

    Update main(4alg).cpp

--- main(4alg).cpp
+++ main(4alg).cpp
@@ -3,8 +3,6 @@
 
 #define filter_tree_it(f) filter_iterator<binary_tree<int, char>::iterator, decltype(f)>
 
-//using namespace std;
-
 auto general_lambda = [](){};
 typedef decltype(general_lambda) lambda_type;
 
@@ -12,7 +10,6 @@ void bin_tree_fucn(){
 	node<int, char> nd1;
 	nd1.data.key = 10; nd1.data.val = 'a';
 	binary_tree<int, char> bt;
-	//bt.add(nd1);
 	bt.add(10, 'a');
 	bt.add(8, 'b');
 	bt.add(7, 'c');
@@ -24,15 +21,13 @@ void bin_tree_fucn(){
 	bt.range_for([](node<int, char> &nd){ print(nd); }, &base(bt), 8, 11);
 	bt.for_each([](node<int, char> &nd){ print(nd); }, &base(bt));
 
-	//std::for_each(bt.begin(), bt.end(), [](qnode<node<int, char>*> nd){ print(*nd.obj); });
-
 	auto _f = [](qnode<node<int, char>*> qn){ if (qn.obj->data.key > 8) std::cout << qn.obj->data.key << " is bigger than 8" << std::endl; };
 	auto _g = [](binary_tree<int, char>::iterator bt_i){ std::cout << (*bt_i).obj->data.key << " is bigger than 8" << std::endl; };
 
 
 	typedef filter_iterator<binary_tree<int, char>::iterator, lambda_type> filter_tree_iter;
 
-	auto _u = [](binary_tree<int, char>::iterator bt_i, bool &b){ b = (*bt_i).obj->data.key == 9; };
+	auto _u = [](binary_tree<int, char>::iterator bt_i){ return (*bt_i).obj->data.key > 8; };
 
 	bt.print_tree();
 
@@ -75,7 +70,6 @@ int main(){
 	bin_tree_fucn();
 	stack_func();
 	queue_func();
-
 
 	return 0;
 }
