73c286eb60958fccf3e6da34d7aee4b51ed581a9
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2019-04-12 00:31:55 +0300

    Update main.cpp

--- jim_laba/main.cpp
+++ jim_laba/main.cpp
@@ -1,47 +1,46 @@
 #include "LinkedList.h"
 #include "BinaryTree.h"
+#include "Graph.h"
 
 void bin_tree(){
 	binary_tree<int, char> tree1;
 	int sz;
-	tree1.add(3, 'c');
-	tree1.add(1, 'a');
-	tree1.add(2, 'b');
-	tree1.add(5, 'e');
-	tree1.add(4, 'd');
-	tree1.add(7, 'e');
-	tree1.add(6, 'e');
+	tree1.add(30, 'c');
+	tree1.add(10, 'a');
+	tree1.add(20, 'b');
+	tree1.add(50, 'e');
+	tree1.add(40, 'd');
+	tree1.add(70, 'e');
+	tree1.add(60, 'e');
 
 	subtree_size(tree1.get_base()->r->r, &sz);
 	std::cout << "size: " << sz << std::endl;
 
-	problem_pred(tree1.get_base(), case_8<int, char>);
-	print(tree1.get_base()->l->r);
+	problem_pred(tree1.get_base(), case_10<int, char>);
+	print(tree1.get_base()->r->r->r->r);
 }
 
 void link_list1(){
 	linked_list<int, char> list1;
 	linked_list<int, char> list2;
-	node<int, char> nd1;
 
+	list1.end_add(node<int, char>(3, 'c'));
 	list1.end_add(node<int, char>(1, 'a'));
 	list1.end_add(node<int, char>(2, 'b'));
 	list1.end_add(node<int, char>(3, 'c'));
 	//list1.end_add(node<int, char>(4, 'd'));
-	list1.end_add(node<int, char>(4, 'd'));
-	list1.end_add(node<int, char>(5, 'e'));
-	list1.end_add(node<int, char>(6, 'f'));
-	//list1.end_add(node<int, char>(7, 'g'));
-	//list1.beg_add(node<int, char>(3, 'd'));
-	//list1.delete_node(list1.get_base());
-
-	//list1.del_lastmax();
-	//list1.del_lastval('b');
+	//list1.end_add(node<int, char>(5, 'e'));
+	//list1.end_add(node<int, char>(6, 'f'));
 	cyclic_left(list1, 2);
-
 	list1.print();
 
-	std::cout << list1.elem_cnt() << std::endl;
+	list2.end_add(node<int, char>(1, 'a'));
+	list2.end_add(node<int, char>(2, 'b'));
+	list2.end_add(node<int, char>(3, 'c'));
+	//list2.end_add(node<int, char>(4, 'd'));
+	//list2.end_add(node<int, char>(5, 'e'));
+	//list2.end_add(node<int, char>(6, 'f'));
+	prt_truth(list1 == list2);
 }
 
 void link_list2(){
@@ -55,9 +54,15 @@ void link_list2(){
 	std::cout << list1.elem_cnt() << std::endl;
 }
 
+void var3(){
+	Task1();
+	Task16(3);
+}
+
 int main(){
 	//link_list1();
-	link_list2();
+	//link_list2();
 	//bin_tree();
 
+	var3();
 }
