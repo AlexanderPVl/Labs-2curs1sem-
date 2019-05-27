3579b8944fa88242ce155c289282e094a94b9d78
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2019-05-28 00:26:26 +0300

    Update algorithms.h

--- laba8/algorithms.h
+++ laba8/algorithms.h
@@ -9,6 +9,8 @@ void copy_tf(T* to, T* from, int n){
 	}
 }
 
+// returns type of parameter if it equals to expected else returns decltype(nan)
+
 template<class T>
 T is_of_type(T*) { return; }
 
@@ -18,11 +20,26 @@ decltype(nan) is_of_type(void*) { return nan; }
 template<class T>
 decltype(nan) is_of_type(...) { return nan; }
 
+// do_if_of_type
+
+template<class T>
+void do_if_of_type(T*) { return; }
+
+template<class T>
+void do_if_of_type(void*) { return nan; }
+
+template<class T>
+void do_if_of_type(...) { return nan; }
+
+// returns last element in parameter pack
+
 template<class T>
 T return_last(T t) { return t; }
 
 template<class T, class ... Remainder>
 auto return_last(T t, Remainder ... rem) -> decltype(return_last(rem ...)) { return return_last(rem ...); }
+
+// hz chto takoe, ne rabotaet
 
 template<class T, class U>
 T return_this(T t, T u) { return t; }
